6fe40742bcab6437e9333f63dadde0828716148e
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-01-08 20:49:59 -0300

    fiz algumas alteracoes

---
+++ DripStore_login/script/form.js
@@ -0,0 +1,9 @@
+let form = document.querySelector("form").addEventListener("submit", function (evento) {
+
+    let login = document.querySelector('[type = "text"]').nodeValue;
+    let password = document.querySelector('[type = password]').nodeValue
+
+    console.log(login)
+})
+
+console.log('hello world')
